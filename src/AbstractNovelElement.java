fe74a93b00616781a665747e36ef926ef06503a5
Regulator1904 <ordonezemanuel51@gmail.com>
2020-03-18 21:54:54 -0700

    checkpoint 1

--- src/AbstractNovelElement.java
+++ src/AbstractNovelElement.java
@@ -1,3 +1,30 @@
-abstract class AbstractNovelElement implements NovelElement_IF{
+import java.awt.*;
 
+public abstract class AbstractNovelElement implements NovelElement_IF{
+    private Font font;
+    private CompositeNovelElements parent;
+
+    @Override
+    public CompositeNovelElements getParent() {
+        return parent;
+    }
+
+    protected void setParent(CompositeNovelElements parent){
+        this.parent = parent;
+    }
+
+    @Override
+    public Font getFont() {
+        if(font!=null)
+            return font;
+        else if(parent!=null)
+            return parent.getFont();
+        else
+            return null;
+    }
+
+    @Override
+    public void setFont(Font font) {
+        this.font = font;
+    }
 }
